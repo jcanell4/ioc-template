451edcb990a7276b05443e808fc273cf777c3030
jcanell4 <jcanell4@ioc.cat>
2013-12-13 04:19:59 +0100

    WikiIocresponseGenerator

--- classes/WikiIocResponseGenerator.php
+++ classes/WikiIocResponseGenerator.php
@@ -11,19 +11,21 @@
  * @author Josep Cañellas <jcanell4@ioc.cat>
  */
 class WikiIocResponseGenerator {
-    const DATA_TYPE=0;
-    const TITLE_TYPE=1;
-    const INFO_TYPE=2;
-    const COMMAND_TYPE=3;
-    const ERROR_TYPE=4;
-    const LOGIN_INFO=5;
-    const SECTOK_DATA=6;
-    const CHANGE_DOM_STYLE="change_dom_style"; //domId afectat + propietat de l'estil a modificar + valor 
-    const CHANGE_WIDGET_PROPERTY="change_widget_property"; //widgetId afectat + propietat a modificar + valor 
-    const RELOAD_WIDGET_CONTENT="reaload_widget_content"; //widgetId afectat
-    const ADD_WIDGET_CHILD="add_widget_child"; ////widgetId afectat + widgetId del fill a afegir + tipus de widget a crear + JSON amb els paràmetres per defecte
-    const REMOVE_WIDGET_CHILD="remove_widget_child"; //widgetId afectat + widgetId del fill a eliminar
-    const REMOVE_ALL_WIDGET_CHILDREN="remove_all_widget_children"; //widgetId afectat
+//    const HTML_TYPE=0;
+//    const TITLE_TYPE=1;
+//    const INFO_TYPE=2;
+//    const COMMAND_TYPE=3;
+//    const ERROR_TYPE=4;
+//    const LOGIN_INFO=5;
+//    const SECTOK_DATA=6;
+//    const DATA_TYPE=7;
+//    const PROCESS_DOM_FROM_FUNCTION="process_dom_from_function"; //domId afectat + AMD (true/flase) + nom funcio/modul on es troba la funció + extra prams
+//    const CHANGE_DOM_STYLE="change_dom_style"; //domId afectat + propietat de l'estil a modificar + valor 
+//    const CHANGE_WIDGET_PROPERTY="change_widget_property"; //widgetId afectat + propietat a modificar + valor 
+//    const RELOAD_WIDGET_CONTENT="reaload_widget_content"; //widgetId afectat
+//    const ADD_WIDGET_CHILD="add_widget_child"; ////widgetId afectat + widgetId del fill a afegir + tipus de widget a crear + JSON amb els paràmetres per defecte
+//    const REMOVE_WIDGET_CHILD="remove_widget_child"; //widgetId afectat + widgetId del fill a eliminar
+//    const REMOVE_ALL_WIDGET_CHILDREN="remove_all_widget_children"; //widgetId afectat
     
     //login (true/false)
     //load page (page_id, page_content)
