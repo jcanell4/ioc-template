b2d90621279f8773f8ba9bb629f17c68f47aca95
jcanell4 <jcanell4@ioc.cat>
2014-05-08 03:22:11 +0200

    eliminar conflicte quan el template no fa servir AjaxCommand

--- cmd_response_handler/WikiIocResponseHandler.php
+++ cmd_response_handler/WikiIocResponseHandler.php
@@ -12,22 +12,47 @@
  */
 
 if (!defined("DOKU_INC")) die();
-if (!defined('DOKU_TPL_CLASSES')) define('DOKU_TPL_CLASSES', tpl_incdir().'classes/');
+
+function _tplIncDir(){
+    global $conf;
+    if(is_callable('tpl_incdir')){
+        $ret=  tpl_incdir();
+    }else{
+        $ret=DOKU_INC.'lib/tpl/'.$conf['template'].'/';
+    }
+    return $ret;
+}
+
+if (!defined('DOKU_TPL_CLASSES')) define('DOKU_TPL_CLASSES', _tplIncDir().'classes/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'ajaxcommand/AbstractResponseHandler.php');
+require_once (_tplIncDir().'conf/mainCfg.php');
 
 abstract class WikiIocResponseHandler extends AbstractResponseHandler {
     function __construct($cmd) {
         parent::__construct($cmd);
     }
-
-    protected function postResponse($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
-        $data = array(
+    
+    private function _getDataEvent(&$ajaxCmdResponseGenerator, 
+                                    $requestParams=NULL, 
+                                    $responseData=NULL){
+        $ret = array(
             "command" => $this->getCommandName(),
             "requestParams" => $requestParams,
             "responseData" => $responseData,
             "ajaxCmdesponseGenerator" => $ajaxCmdResponseGenerator,
-        );
+            "tplComponents" => WikiIocCfg::Instance(), /*modificar l'objecte WikiIocCfg per TplComponents*/
+        );  
+        return $ret;
+        
+    }
+
+    protected function postResponse($requestParams, 
+                                        $responseData, 
+                                        &$ajaxCmdResponseGenerator) {
+        $data = $this->_getDataEvent($ajaxCmdResponseGenerator, 
+                                        $requestParams, 
+                                        $responseData);
         $evt = new Doku_Event("WIOC_PROCESS_RESPONSE", $data);
         $evt->advise_after();
         unset($evt);
@@ -37,11 +62,7 @@ abstract class WikiIocResponseHandler extends AbstractResponseHandler {
     }
 
     protected function preResponse($requestParams, &$ajaxCmdResponseGenerator) {
-        $data = array(
-            "command" => $this->getCommandName(),
-            "requestParams" => $requestParams,
-            "ajaxCmdesponseGenerator" => $ajaxCmdResponseGenerator,
-        );
+        $data = $this->_getDataEvent($ajaxCmdResponseGenerator, $requestParams);
         $evt = new Doku_Event("WIOC_PROCESS_RESPONSE", $data);
         $ret = $evt->advise_before();
         unset($evt);
