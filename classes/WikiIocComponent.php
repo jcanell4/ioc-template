9254621eb9042493c96dfdc495b83675838e83e9
rclaver <rclaver@xtec.cat>
2014-10-28 10:07:50 +0100

    change: normalització dels paràmetres de les classes

--- classes/WikiIocComponent.php
+++ classes/WikiIocComponent.php
@@ -12,26 +12,34 @@ require_once(DOKU_TPL_INCDIR . 'conf/js_packages.php');
  * @author Josep Cañellas <jcanell4@ioc.cat>
  */
 abstract class WikiIocComponent extends WikiIocBuilder {
-    private $id;
-    private $label;
-    private $toolTip;
-    private $selected;
+    private $aParams;   //array de paràmetres
 
     /**
-     * @param string  $label       etiqueta del component
-     * @param string  $id          id del component
+     * @param array[] $aParms      hash amb els paràmetres del component
      * @param array[] $reqPackage  hash amb els packages requerit amb el format:
-     *                             "array("name" => "ioc", "location" => $js_packages["ioc"])
+     *                             array("name" => "ioc", "location" => $js_packages["ioc"])
      */
-    function __construct($aParms = array("id" => NULL, "label" => ""), $reqPackage = array()) {
+    function __construct($aParms = array(), $reqPackage = array()) {
         parent::__construct($reqPackage);
-        $this->id       = $aParms['id'];
-        $this->label    = $aParms['label'];
-        $this->toolTip  = $aParms['label'];
-        $this->selected = FALSE;
+        
+        $this->aParams = $aParms;
+        
+        if (!$this->aParams['selected']) {
+            $this->aParams['selected'] = FALSE;
+        }
+        if ($this->aParams['label'] && !$this->aParams['toolTip']) {
+            $this->aParams['toolTip'] = $this->aParams['label'];
+        }
     }
 
     public abstract function getRenderingCode();
+
+    function get($key) {
+        return $this->aParams[$key];
+    }
+    function set($key, $value) {
+        $this->aParams[$key] = $value;
+    }
 
     /**
      * Retorna la etiqueta del component.
@@ -39,69 +47,62 @@ abstract class WikiIocComponent extends WikiIocBuilder {
      * @return string
      */
     function getLabel() {
-        return $this->label;
+        return $this->get('label');
     }
-
     /**
      * Retorna la id del component.
      *
      * @return string
      */
     function getId() {
-        return $this->id;
+        return $this->get('id');
     }
-
     /**
      * Retorna el tooltip del component.
      *
      * @return string
      */
     function getToolTip() {
-        return $this->toolTip;
+        return $this->get('toolTip');
     }
-
     /**
      * Retorna true si es seleccionat o false en cas contrari
      *
      * @return bool
      */
     function isSelected() {
-        return $this->selected;
+        return $this->get('selected');
     }
-
     /**
      * Estableix la etiqueta del component.
      *
      * @param string $label
      */
-    function setLabel($label) {
-        $this->label = $label;
+    function setLabel($v) {
+        $this->set('label', $v);
     }
-
     /**
      * Estableix la id del component.
      *
      * @param string $id
      */
-    function setId($id) {
-        $this->id = $id;
+    function setId($v) {
+        $this->set('id', $v);
     }
-
     /**
      * Estableix el tooltip del component.
      *
      * @param string $tip
      */
-    function setToolTip($tip) {
-        $this->toolTip = $tip;
+    function setToolTip($v) {
+        $this->set('toolTip', $v);
     }
-
     /**
      * Estableix que el component es seleccionat si es true o no seleccionat si es false.
      *
      * @param bool $value
      */
-    function setSelected($value) {
-        $this->selected = $value;
+    function setSelected($v) {
+        $this->set('selected', $v);
     }
 }
