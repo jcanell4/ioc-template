fc65021ae744ce3784d371a8ca1dda298d4ce140
eduard <eduard@eduard-Minitower.(none)>
2015-01-22 13:26:07 +0100

    Login use getAdminTaskList()

--- cmd_response_handler/LoginResponseHandler.php
+++ cmd_response_handler/LoginResponseHandler.php
@@ -13,17 +13,19 @@ require_once DOKU_PLUGIN.'ajaxcommand/JsonGenerator.php';
 require_once tpl_incdir()."conf/mainCfg.php";
 
 class LoginResponseHandler extends WikiIocResponseHandler {
-    
+
     function __construct() {
         parent::__construct(WikiIocResponseHandler::LOGIN);
     }
-    
+
     protected function response($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
         $this->login($requestParams, $responseData, $ajaxCmdResponseGenerator);
     }
 
     private function login ($requestParams, $responseData, &$ajaxCmdResponseGenerator){
-        $ajaxCmdResponseGenerator->addLoginInfo($responseData["loginRequest"], 
+
+
+        $ajaxCmdResponseGenerator->addLoginInfo($responseData["loginRequest"],
                                                 $responseData['loginResult'],
                                                 $responseData['userId']);
         $ajaxCmdResponseGenerator->addSectokData(getSecurityToken());
@@ -33,17 +35,18 @@ class LoginResponseHandler extends WikiIocResponseHandler {
             $ajaxCmdResponseGenerator->addSetJsInfo($this->getJsInfo());
             $ajaxCmdResponseGenerator->addReloadWidgetContent($cfg->getArrIds("zN_index_id"));
             $ajaxCmdResponseGenerator->addChangeWidgetProperty(
-                                                    $cfg->getArrIds("userButton"), 
-                                                    "label", 
+                                                    $cfg->getArrIds("userButton"),
+                                                    "label",
                                                     $responseData["userId"]);
-           $title = "title_login";
-           $content = "content_login";
-           $urlBase = "urlBase_login";
+
+            $dades = $this->getModelWrapper()->getAdminTaskList();
+
+            $urlBase = "urlBase_login";
 
             $ajaxCmdResponseGenerator->addAdminTab($cfg->getArrIds("zonaNavegacio"),
                                                    $cfg->getArrIds("zN_admin_id"),
-                                                   $title,
-                                                   $content, 
+                                                   $dades['title'],
+                                                   $dades['content'],
                                                    $urlBase);
             $title = $_SERVER['REMOTE_USER'];
             $sig = toolbar_signature();
@@ -56,5 +59,30 @@ class LoginResponseHandler extends WikiIocResponseHandler {
         }
         $ajaxCmdResponseGenerator->addTitle($title);
         $ajaxCmdResponseGenerator->addProcessFunction(true, "ioc/dokuwiki/setSignature", $sig);
-    }    
+
+
+        global $lang;
+
+        $info = array('id' => '', 'duration' => -1, 'timestamp' => date('d-m-Y H:i:s'));
+
+        if ($responseData['loginRequest'] && !$responseData['loginResult']) {
+            $info['type'] = 'error';
+            //$info['message'] = $lang['auth_error'];
+            $info['message'] = 'Error d\'autenticació';
+
+
+        } else if (!$responseData['loginRequest'] && !$responseData['loginResult']) {
+            $info['type'] = 'info';
+            //$info['message'] = $lang['user_logout'];
+            $info['message'] = 'Usuari desconnectat';
+
+
+        } else  {
+            $info['type']= 'success';
+            // $info['message'] = $lang['user_login'];
+            $info['message'] = 'Usuari connectat';
+        }
+
+        $ajaxCmdResponseGenerator->addInfoDta($info);
+    }
 }
