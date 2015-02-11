9242653bdadeb3b7b2720dda22fda592b5c0ddcf
eduard <eduard@eduard-Minitower.(none)>
2015-02-11 20:24:42 +0100

    update response function

--- cmd_response_handler/Admin_taskResponseHandler.php
+++ cmd_response_handler/Admin_taskResponseHandler.php
@@ -8,6 +8,7 @@
 if (!defined("DOKU_INC")) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(tpl_incdir().'cmd_response_handler/WikiIocResponseHandler.php');
+//require_once(tpl_incdir().'cmd_response_handler/PageResponseHandler.php');
 require_once DOKU_PLUGIN.'ajaxcommand/JsonGenerator.php';
 
 class Admin_taskResponseHandler extends WikiIocResponseHandler {
@@ -19,13 +20,14 @@ class Admin_taskResponseHandler extends WikiIocResponseHandler {
                                 &$ajaxCmdResponseGenerator) {
         //TODO La informació ha de venir de DokuModelAdapter. Cal fer el canvi
         $responseData["info"] = "ADMIN TASK ";
-        error_log("Admin_taskResponseHandler\n", 3, "/var/www/php.log");
-
-        parent::response($requestParams,
-                        $responseData,
-                        $ajaxCmdResponseGenerator);
-      //$ajaxCmdResponseGenerator->addProcessFunction(true,
-      //                                      "ioc/dokuwiki/processCancellation");
+        //error_log("AAdmin_taskResponseHandler\n", 3, "/var/www/php.log");
+        //error_log(print_r($requestParams,true), 3, "/var/www/php.log");
+        if($requestParams['page']){
+          $ajaxCmdResponseGenerator->addHtmlDoc($responseData['id'],
+                                                $responseData['ns'],
+                                                $responseData['title'],
+                                                $responseData['content']);
+        }
     }
 }
 
