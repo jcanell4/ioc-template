b702afeb1b55820c1b3fedcd344546a5c833f765
eduard <eduard@eduard-Minitower.(none)>
2015-03-03 09:46:04 +0100

    Afegit case "plugin"

--- cmd_response_handler/Admin_taskResponseHandler.php
+++ cmd_response_handler/Admin_taskResponseHandler.php
@@ -8,7 +8,6 @@
 if (!defined("DOKU_INC")) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(tpl_incdir().'cmd_response_handler/WikiIocResponseHandler.php');
-//require_once(tpl_incdir().'cmd_response_handler/PageResponseHandler.php');
 require_once DOKU_PLUGIN.'ajaxcommand/JsonGenerator.php';
 
 class Admin_taskResponseHandler extends WikiIocResponseHandler {
@@ -19,30 +18,47 @@ class Admin_taskResponseHandler extends WikiIocResponseHandler {
                                 $responseData,
                                 &$ajaxCmdResponseGenerator) {
         if($requestParams['page']){
-          switch($requestParams['page']) {
-            case "acl":
+            // afegeix la pestanya al panell central
             $ajaxCmdResponseGenerator->addAdminTask($responseData['id'],
-                                                   $responseData['ns'],
-                                                   $responseData['title'],
-                                                   $responseData['content']);
+                                               $responseData['ns'],
+                                               $responseData['title'],
+                                               $responseData['content']);
 
             // missatge a mostrar al panell inferior
             $ajaxCmdResponseGenerator->addInfoDta($responseData['info']);
 
-            // Obté els Selectors Css dels forms del pluguin ACL
-            $params = array();
-            $this->getAclSelectors($params);
+            switch($requestParams['page']) {
+                case "acl":
+                    // Obté els Selectors Css dels forms del pluguin ACL
+                    $params = array();
+                    $this->getAclSelectors($params);
 
-            $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                $responseData['id'],
-                true,
-                "ioc/dokuwiki/processAclTask",
-                array(
-                  "urlBase" => "lib/plugins/ajaxcommand/ajax.php?call=admin_task",
-                  "saveSelector" => $params["saveSelector"],
-                  "updateSelector" => $params["updateSelector"]
-                )
-            );
+                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
+                        $responseData['id'],
+                        true,
+                        "ioc/dokuwiki/processAclTask",
+                        array(
+                            "urlBase" => "lib/plugins/ajaxcommand/ajax.php?call=admin_task",
+                            "saveSelector" => $params["saveSelector"],
+                            "updateSelector" => $params["updateSelector"]
+                        )
+                    );
+                break;
+                case "plugin":
+                    // Obté els Selectors Css dels forms del plugin PLUGIN
+                    $params = array();
+                    $this->getPluginSelectors($params);
+
+                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
+                        $responseData['id'],
+                        true,
+                        "ioc/dokuwiki/processPluginTask",
+                        array(
+                           "urlBase" => "lib/plugins/ajaxcommand/ajax.php?call=admin_task",
+                           "commonSelector" => $params["commonSelector"],
+                           "pluginsSelector" => $params["pluginsSelector"]
+                        )
+                    );
             break;
           }
       }
