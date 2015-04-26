e0edc3f5b9ad2efc5685ff117771332178de97a5
eduard <eduard@eduard-Minitower.(none)>
2015-04-26 09:33:56 +0200

    New_pageResponseHandler extends pageResponseHandler

--- cmd_response_handler/New_pageResponseHandler.php
+++ cmd_response_handler/New_pageResponseHandler.php
@@ -8,44 +8,20 @@
 
 if (!defined("DOKU_INC")) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(tpl_incdir().'cmd_response_handler/WikiIocResponseHandler.php');
+require_once(tpl_incdir().'cmd_response_handler/PageResponseHandler.php');
 require_once DOKU_PLUGIN.'ajaxcommand/JsonGenerator.php';
 
-class New_pageResponseHandler extends WikiIocResponseHandler {
-    function __construct() {
-        parent::__construct(WikiIocResponseHandler::PAGE);
+class New_pageResponseHandler extends pageResponseHandler
+{
+    function __construct()
+    {
+        parent::__construct(pageResponseHandler::PAGE);
     }
-    protected function response($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
-        $ajaxCmdResponseGenerator->addHtmlDoc($responseData['id'], 
-                                                $responseData['ns'], 
-                                                $responseData['title'], 
-                                                $responseData['content']);
-        
-        $metaData = $this->getModelWrapper()->getMetaResponse($responseData['id']);
-        $ajaxCmdResponseGenerator->addMetadata($metaData['id'],
-                                                $metaData['meta']);
 
+    protected function response($requestParams, $responseData, &$ajaxCmdResponseGenerator)
+    {
+        parent::response($requestParams, $responseData, $ajaxCmdResponseGenerator);
+    }
 
-        $ajaxCmdResponseGenerator->addInfoDta($responseData["info"]);
-
-
-        $id = $metaData['id'];
-        $revs = $this->getModelWrapper()->getRevisions($id);
-        $ajaxCmdResponseGenerator->addRevisionsTypeResponse($id, $revs);
-
-
-        $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-            $responseData['id'],
-            true,
-            "ioc/dokuwiki/processContentPage",  //TODO configurable
-            array(
-                "ns" => $responseData['ns'], 
-                "editCommand" => "lib/plugins/ajaxcommand/ajax.php?call=edit",
-                "detailCommand" => "lib/plugins/ajaxcommand/ajax.php?call=get_image_detail",
-            )
-        );
-
-    }    
 }
-
 ?>
