2c293d37ef0db8d5ea9e1d2c8f7e93bf8c994cd3
jcanell4 <jcanell4@xtec.cat>
2015-05-18 14:44:52 +0200

    Fixing error

--- conf/amdFile.js
+++ conf/amdFile.js
@@ -30,7 +30,7 @@ var processorUser = new ErrorMultiFunctionProcessor();
 var requestUser = new Request();
 requestUser.urlBase = "lib/plugins/ajaxcommand/ajax.php?call=new_page";
 processorUser.addErrorAction("1001", function () {
-requestUser.sendRequest(userDialog.getQuery);
+requestUser.sendRequest(userDialog.getQuery());
 });
 userDialog.addProcessor(processorUser.type, processorUser);
 }
@@ -46,7 +46,7 @@ var processorTalk = new ErrorMultiFunctionProcessor();
 var requestTalk = new Request();
 requestTalk.urlBase = "lib/plugins/ajaxcommand/ajax.php?call=new_page";
 processorTalk.addErrorAction("1001", function () {
-requestTalk.sendRequest(userDialog.getQuery);
+requestTalk.sendRequest(userDialog.getQuery());
 });
 userDialog.addProcessor(processorTalk.type, processorTalk);
 }
