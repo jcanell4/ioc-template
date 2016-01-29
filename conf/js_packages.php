a1f037bdc95e6d8edbd5499c5a8ac04ea1cff0ec
jcanell4 <jcanell4@xtec.cat>
2016-01-29 01:39:45 +0100

    correcció del codi. Ara $conf serà sempre local

--- conf/js_packages.php
+++ conf/js_packages.php
@@ -5,13 +5,19 @@
  */
 
 if (!defined('DOKU_INC')) die();
-require_once DOKU_INC.'lib/plugins/ownInit/conf/default.php';
 
-global $js_packages;
-$js_packages["ioc"]="/iocjslib/ioc";
-$js_packages["ace-builds"]="/ace-builds/src-noconflict";
+f_js_packages();
 
-$js_packages["dojo"]=$conf['dojo_url_base']."dojo";
-$js_packages["dijit"]=$conf['dojo_url_base']."dijit";
-$js_packages["dojox"]=$conf['dojo_url_base']."dojox";
+function f_js_packages(){
+    $conf = array();
+    include DOKU_INC.'lib/plugins/ownInit/conf/default.php';
+
+    global $js_packages;
+    $js_packages["ioc"]="/iocjslib/ioc";
+    $js_packages["ace-builds"]="/ace-builds/src-noconflict";
+
+    $js_packages["dojo"]=$conf['dojo_url_base']."dojo";
+    $js_packages["dijit"]=$conf['dojo_url_base']."dijit";
+    $js_packages["dojox"]=$conf['dojo_url_base']."dojox";
+}
 ?>
