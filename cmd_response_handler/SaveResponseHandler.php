bed26fa3efb5b25c3dfa4c29ad1f4df4b0a4cc28
rclaver <rclaver@xtec.cat>
2016-03-07 13:16:32 +0100

    Evaluar l'estat deleted d'una pàgina

--- cmd_response_handler/SaveResponseHandler.php
+++ cmd_response_handler/SaveResponseHandler.php
@@ -10,6 +10,7 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(tpl_incdir().'cmd_response_handler/PageResponseHandler.php');
 //require_once(tpl_incdir().'cmd_response_handler/EditResponseHandler.php');
 require_once DOKU_PLUGIN.'ajaxcommand/JsonGenerator.php';
+require_once(DOKU_PLUGIN.'ajaxcommand/requestparams/PageKeys.php');
 
 //class SaveResponseHandler extends EditResponseHandler {
 class SaveResponseHandler extends PageResponseHandler {
@@ -22,18 +23,20 @@ class SaveResponseHandler extends PageResponseHandler {
                                 &$ajaxCmdResponseGenerator) {
 
         // TODO[Xavi] Com els errors es gestionen amb excepcions, cal fer servir el code? Si es llença excepció no arriba aquí
-        if($responseData["code"]==0){
+        if ($responseData["code"]==0 && !$responseData["deleted"]){
             $ajaxCmdResponseGenerator->addInfoDta($responseData["info"]);
-            $ajaxCmdResponseGenerator->addProcessFunction(true, 
-                                            "ioc/dokuwiki/processSaving");
+            $ajaxCmdResponseGenerator->addProcessFunction(true, "ioc/dokuwiki/processSaving");
             $params = array(
                 "formId" => $responseData["formId"],
                 "inputs" => $responseData["inputs"],
             );
-            $ajaxCmdResponseGenerator->addProcessFunction(true, 
-                                        "ioc/dokuwiki/processSetFormInputValue",
-                                        $params);
-        }else{
+            $ajaxCmdResponseGenerator->addProcessFunction(true, "ioc/dokuwiki/processSetFormInputValue", $params);
+        }
+        elseif ($responseData["deleted"]){
+            $ajaxCmdResponseGenerator->addRemoveContentTab($requestParams[PageKeys::KEY_ID]);
+            $ajaxCmdResponseGenerator->addAlert($responseData["info"]['message']);
+        }
+        else{
             $ajaxCmdResponseGenerator->addError($responseData["code"], 
                                                 $responseData["info"]);
             $ajaxCmdResponseGenerator->addProcessFunction(true, 
