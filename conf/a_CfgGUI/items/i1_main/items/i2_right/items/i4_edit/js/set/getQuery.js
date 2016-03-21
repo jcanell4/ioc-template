52dc1b5049a2c56a81b8c87712c6ae30b1d531cb
Xavier García <xaviergaro.dev@gmail.com>
2016-03-21 12:06:28 +0100

    Added structured & full last local draft time to every edit query

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit/js/set/getQuery.js
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit/js/set/getQuery.js
@@ -1,9 +1,9 @@
 _ret="";
 if (this.dispatcher.getGlobalState().currentTabId) {
-   var ns=this.dispatcher.getGlobalState().getContent(
-                            this.dispatcher.getGlobalState().currentTabId)["ns"];
+    var globalState = this.dispatcher.getGlobalState();
+   var ns=globalState.getContent(globalState.currentTabId).ns;
 
-    var rev = this.dispatcher.getGlobalState().getCurrentContent().rev;
+    var rev = globalState.getCurrentContent().rev;
 
     if(this.query){
        _ret=this.query + "&id=" + ns;
@@ -14,4 +14,6 @@ if (this.dispatcher.getGlobalState().currentTabId) {
     if (rev) {
         _ret+="&rev=" + rev;
     }
+
+    _ret+=this.dispatcher.getDraftManager().generateLastLocalDraftTimesParam(globalState.currentTabId)
 }
