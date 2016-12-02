b31b0a3cc1539f8e29a3a996416926847ebc48d7
jcanell4 <jcanell4@xtec.cat>
2016-12-02 03:27:34 +0100

    Fixed error saving navigationId in globalState

--- conf/amdFile.js
+++ conf/amdFile.js
@@ -60,11 +60,7 @@ var wikiIocDispatcher = getDispatcher();
 var tbContainer = registry.byId(wikiIocDispatcher.navegacioNodeId);
 if (tbContainer) {
 tbContainer.watch("selectedChildWidget", function (name, oldTab, newTab) {
-var documentId = globalState.getCurrentId();
-var contentCache = wikiIocDispatcher.getContentCache(documentId);
-if (contentCache) {
-contentCache.setCurrentId("navigationPane", newTab.id);
-}
+wikiIocDispatcher.getGlobalState().setCurrentNavigationId(newTab.id);
 if (newTab.updateRendering) {
 newTab.updateRendering();
 }
