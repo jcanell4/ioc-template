728e30c5022c47fa77b2cca440adcb1347d83d8b
Xavier García <xaviergaro.dev@gmail.com>
2017-01-17 22:37:33 +0100

    Fixed script loading error

--- classes/WikiIocTpl.php
+++ classes/WikiIocTpl.php
@@ -134,6 +134,9 @@ class WikiIocTpl {
         );
         array_unshift($event->data["script"], $item1);
         array_unshift($event->data["script"], $item0);
+
+        // TODO[Xavi] Establir cookie
+        setcookie("IOCForceScriptLoad", "1");
     }
 
     public function printHeaderTags() {
