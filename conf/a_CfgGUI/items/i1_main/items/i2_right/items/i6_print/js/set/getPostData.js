fdd541691746c5210c67c6ce359946e801b1a98e
jcanell4 <jcanell4@xtec.cat>
2017-05-16 00:38:22 +0200

    Changed name of getCurrentSection as getCurrentHeaderId

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/i6_print/js/set/getPostData.js
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/i6_print/js/set/getPostData.js
@@ -9,7 +9,7 @@ if (this.dispatcher.getGlobalState().currentTabId) {
                 && this.dispatcher.getChangesManager().isContentChanged(id)){
         hasChanges = 1;
    }else if(this.dispatcher.getGlobalState().getCurrentContent().action==='sec_edit'
-                && this.dispatcher.getChangesManager().isContentChanged(id)){
+                && this.dispatcher.getChangesManager().isChanged(id)){
        hasChanges = 2;
    }else{
        hasChanges = 0;
@@ -20,19 +20,22 @@ if (this.dispatcher.getGlobalState().currentTabId) {
             id: ns,
             wikitext:this.dispatcher.getWidget(id).getQuerySave().wikitext
         };
+        /*console.log("call:preview, id:"+ns+"wikitext:" + _ret.wikitext);*/
    }else if(hasChanges==2){
        var editor  =this.dispatcher.getWidget(id);
-       var queryValues = editor.getQuerySave(editor.getCurrentSection());
+       var queryValues = editor.getQuerySave(editor.getCurrentHeaderId());
         _ret={
             call:'preview',
             id: ns,
             wikitext:queryValues.prefix+queryValues.wikitext+queryValues.suffix
         };
+        /*console.log("call:preview, id:"+ns+"wikitext:" + _ret.wikitext);*/
    }else{
         _ret={
             call:'print',
             id: ns,
         };
+        /*console.log("call:print, id:"+ns);*/
    }
    if (rev) {
         _ret.rev = rev;
