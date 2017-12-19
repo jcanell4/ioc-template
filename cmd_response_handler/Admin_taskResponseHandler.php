8b62a7ffe0f8f74f96299f7856ced4f1f9ee225c
rclaver <rclaver@xtec.cat>
2017-12-19 11:33:09 +0100

    psé

--- cmd_response_handler/Admin_taskResponseHandler.php
+++ cmd_response_handler/Admin_taskResponseHandler.php
@@ -8,6 +8,7 @@ require_once(DOKU_INC . 'lib/plugins/ajaxcommand/defkeys/ResponseHandlerKeys.php
 require_once(DOKU_TPL_INCDIR.'cmd_response_handler/WikiIocResponseHandler.php');
 
 class Admin_taskResponseHandler extends WikiIocResponseHandler {
+    
     function __construct() {
         parent::__construct(ResponseHandlerKeys::ADMIN_TASK);
     }
