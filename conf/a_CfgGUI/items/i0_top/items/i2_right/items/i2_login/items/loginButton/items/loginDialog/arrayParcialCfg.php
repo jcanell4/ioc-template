73c562fe48f399d4b267bec6afeb81ac446bb3d8
jcanell4 <jcanell4@xtec.cat>
2018-02-15 01:56:11 +0100

    We have been ensured that the method of sending the login is post

--- conf/a_CfgGUI/items/i0_top/items/i2_right/items/i2_login/items/loginButton/items/loginDialog/arrayParcialCfg.php
+++ conf/a_CfgGUI/items/i0_top/items/i2_right/items/i2_login/items/loginButton/items/loginDialog/arrayParcialCfg.php
@@ -6,6 +6,7 @@ $arrParcial = array(
                       			"id" => cfgIdConstants::LOGIN_DIALOG
                                     ),
                             "DJO" => array(
+                      			"method" => "'post'",
                       			"urlBase" => "'lib/exe/ioc_ajax.php?call=login'",
                                         "standbyId" => "'loginDialog_hidden_container'"
                                     )
