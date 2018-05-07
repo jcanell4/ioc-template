14cb658a5de4b7d5c90a8fc4850ec8b83b8faef0
rclaver <rclaver@xtec.cat>
2018-05-07 08:55:31 +0200

    hasDraft

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit_project/js/set/getQuery.js
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit_project/js/set/getQuery.js
@@ -1,20 +1,15 @@
 _ret="";
-
 if (this.dispatcher.getGlobalState().currentTabId) {
    var globalState=this.dispatcher.getGlobalState();
    var ns=globalState.getContent(globalState.currentTabId).ns;
    var rev=globalState.getCurrentContent().rev;
    var projectType=globalState.getContent(globalState.getCurrentId()).projectType;
+   var hasDraft = !jQuery.isEmptyObject(this.dispatcher.getDraftManager().getContentLocalDraft(ns));
    
-   if(this.query){
-      _ret=this.query+"&";
-   }
+   if(this.query) _ret=this.query+"&";
    _ret+="id="+ns+"&projectType="+projectType;
-
-   if(rev) {
-      _ret+="&rev="+rev;
-   }
-
+   if(hasDraft) _ret+="&hasDraft=true";
+   if(rev) _ret+="&rev="+rev;
    _ret+=this.dispatcher.getDraftManager().generateLastLocalDraftTimesParam(globalState.currentTabId, ns);
    _ret+="&contentFormat="+globalState.userState['editor'];
 }
