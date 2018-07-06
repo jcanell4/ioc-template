83f8a86b23649bc763eeb828fbb850d35de85242
rclaver <rclaver@xtec.cat>
2018-07-06 11:57:27 +0200

    prueba (inútil) para corregir standbyId

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit/arrayParcialCfg.php
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/i4_edit/arrayParcialCfg.php
@@ -12,9 +12,9 @@ $arrParcial = array(
                                           ,"autoSize" => true
                                           ,"visible" => false
                                           ,'iconClass' => "'iocIconEdit'"
-                                          ,"standbyId" => cfgIdConstants::getConstantToString(cfgIdConstants::BODY_CONTENT)
+                                          ,"standbyId" => cfgIdConstants::BODY_CONTENT
                                           ,"urlBase" => "'lib/exe/ioc_ajax.php?call=edit'"
-                                          ,'disableOnSend' => true                                  
+                                          ,'disableOnSend' => true
                                         )
                             )
               );
