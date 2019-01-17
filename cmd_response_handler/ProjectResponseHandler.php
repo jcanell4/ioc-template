3a16585caa58a891e5b72eac0fcfb9a9665db070
jcanell4 <jcanell4@xtec.cat>
2019-01-18 00:05:40 +0100

    milllora de l'acció generate

--- cmd_response_handler/ProjectResponseHandler.php
+++ cmd_response_handler/ProjectResponseHandler.php
@@ -117,6 +117,10 @@ class ProjectResponseHandler extends WikiIocResponseHandler {
                 case ProjectKeys::KEY_GENERATE:
                     if ($responseData[ProjectKeys::KEY_INFO])
                         $ajaxCmdResponseGenerator->addInfoDta($responseData[ProjectKeys::KEY_INFO]);
+                    $ajaxCmdResponseGenerator->addExtraContentStateResponse($responseData[ProjectKeys::KEY_ID], ProjectKeys::KEY_GENERATED, $responseData[ProjectKeys::KEY_GENERATED]);
+                    if(!$responseData[ProjectKeys::KEY_GENERATED]){
+                        $ajaxCmdResponseGenerator->addAlert(WikiIocLangManager::getLang("project_not_generated"));
+                    }
                     break;
 
                 case ProjectKeys::KEY_REVERT:
