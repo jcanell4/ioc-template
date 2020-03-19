00f0207db141ea39126bd495d47ddeeb5d40ce5d
rclaver <rclaver@xtec.cat>
2020-03-19 13:34:30 +0100

    racionalización

--- cmd_response_handler/Admin_taskResponseHandler.php
+++ cmd_response_handler/Admin_taskResponseHandler.php
@@ -8,127 +8,67 @@ require_once(DOKU_INC . 'lib/plugins/ajaxcommand/defkeys/ResponseHandlerKeys.php
 require_once(DOKU_TPL_INCDIR.'cmd_response_handler/WikiIocResponseHandler.php');
 
 class Admin_taskResponseHandler extends WikiIocResponseHandler {
-    
+
     function __construct() {
         parent::__construct(ResponseHandlerKeys::ADMIN_TASK);
     }
+
     protected function response($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
         $urlBase = "lib/exe/ioc_ajax.php?call=admin_task";
 
-        if($responseData["needRefresh"]){
+        if ($responseData["needRefresh"]){
             $params = array(
                 "urlBase" => $urlBase,
                 "method" => "post",
                 "query" => "page=".$requestParams["page"],
             );
             $ajaxCmdResponseGenerator->addProcessFunction(TRUE, "ioc/dokuwiki/recallCommand", $params);
-        }else if($requestParams['page']){
+        }
+        else if($requestParams['page']){
             // afegeix la pestanya al panell central
             $ajaxCmdResponseGenerator->addAdminTask($responseData['id'],
-                                               $responseData['ns'],
-                                               $responseData['title'],
-                                               $responseData['content']);
+                                                    $responseData['ns'],
+                                                    $responseData['title'],
+                                                    $responseData['content']);
 
             // missatge a mostrar al panell inferior
             $ajaxCmdResponseGenerator->addInfoDta($responseData['info']);
 
             switch($requestParams['page']) {
                 case "acl":
-                    // Obté els Selectors Css dels forms del pluguin ACL
-                    $params = array();
+                    $params = array('urlBase' => $urlBase);
                     $this->getAclSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processAclTask",
-                        array(
-                            "urlBase" => $urlBase,
-                            "saveSelector" => $params["saveSelector"],
-                            "updateSelector" => $params["updateSelector"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processAclTask";
                     break;
                 case "plugin":
-                    // Obté els Selectors Css dels forms del plugin PLUGIN
-                    $params = array();
+                    $params = array('urlBase' => $urlBase);
                     $this->getPluginSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processPluginTask",
-                        array(
-                           "urlBase" => $urlBase,
-                           "commonSelector" => $params["commonSelector"],
-                           "pluginsSelector" => $params["pluginsSelector"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processPluginTask";
                     break;
                 case "config":
-                    // Obté els Selectors Css dels forms del plugin PLUGIN
-                    $params = array();
+                    $params = array('urlBase' => $urlBase);
                     $this->getConfigSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processConfigTask",
-                        array(
-                           "urlBase" => $urlBase,
-                           "configSelector" => $params["configSelector"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processConfigTask";
                     break;
                 case "usermanager":
-                    // Obté els Selectors Css dels forms del plugin USERMANAGER
-                    $params = array();
+                    $params = array('urlBase' => $urlBase);
                     $this->getUserManagerSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processUserManagerTask",
-                        array(
-                           "urlBase" => $urlBase,
-                           "formsSelector" => $params["formsSelector"],
-                           "exportCsvName" => $params["exportCsvName"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processUserManagerTask";
                     break;
                 case "revert":
-                    // Obté els Selectors Css dels forms del plugin REVERT
-                    $params = array();
+                    $params = array('urlBase' => "lib/exe/ioc_ajax.php?");
                     $this->getRevertSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processRevertTask",
-                        array(
-                           "urlBase" => "lib/exe/ioc_ajax.php?",
-                           "revertSelector" => $params["revertSelector"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processRevertTask";
                     break;
                 case "latex":
-                    // Obté els Selectors Css dels forms del plugin LATEX
-                    $params = array();
+                    $params = array('urlBase' => $urlBase);
                     $this->getLatexSelectors($params);
-
-                    $ajaxCmdResponseGenerator->addProcessDomFromFunction(
-                        $responseData['id'],
-                        true,
-                        "ioc/dokuwiki/processLatexTask",
-                        array(
-                           "urlBase" => $urlBase,
-                           "latexSelector" => $params["latexSelector"],
-                           "latexpurge" => $params["latexpurge"],
-                           "dotest" => $params["dotest"]
-                        )
-                    );
+                    $task = "ioc/dokuwiki/processLatexTask";
                     break;
-          }
+            }
+
+            $ajaxCmdResponseGenerator->addProcessDomFromFunction($responseData['id'], true, $task, $params);
+
       }
     }
 }
