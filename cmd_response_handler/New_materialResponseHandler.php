dac3a12d82940a155c77685b704cb88e7df1828d
rclaver <rclaver@xtec.cat>
2020-05-10 11:33:09 +0200

    New_materialResponseHandler

---
+++ cmd_response_handler/New_materialResponseHandler.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * New_materialResponseHandler
+ */
+if (!defined("DOKU_INC")) die();
+require_once(__DIR__ . "/New_pageResponseHandler.php");
+
+class New_materialResponseHandler extends New_pageResponseHandler {
+
+}
