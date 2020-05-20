fc701a3f17f12d675a9d4c22b21ce931540b299a
rclaver <rclaver@xtec.cat>
2020-05-20 15:39:37 +0200

    hauria de generar un Alert (però no ho fa)

--- cmd_response_handler/New_materialResponseHandler.php
+++ cmd_response_handler/New_materialResponseHandler.php
@@ -7,4 +7,10 @@ require_once(__DIR__ . "/New_pageResponseHandler.php");
 
 class New_materialResponseHandler extends New_pageResponseHandler {
 
+    protected function response($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
+        parent::response($requestParams, $responseData, $ajaxCmdResponseGenerator);
+        if (isset($responseData['alert'])) {
+            $ajaxCmdResponseGenerator->addAlert($responseData['alert']);
+        }
+    }
 }
