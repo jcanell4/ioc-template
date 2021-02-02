682901e3752aa04bf2cfd9f64b99ae66040a68d0
jcanell4 <jcanell4@xtec.cat>
2021-02-02 14:06:40 +0100

    correcció d'errors per descuit

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/iZ9_remove_project/js/set/onClick.js
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/iZ9_remove_project/js/set/onClick.js
@@ -2,4 +2,4 @@ var globalState=this.dispatcher.getGlobalState();
 if (globalState.currentTabId) {
     var id=globalState.getCurrentId();
     this.ok=confirm("Vols eliminar el projecte\n\n\t\'"+globalState.getContent(id).ns+"\'?");
-};
+}
