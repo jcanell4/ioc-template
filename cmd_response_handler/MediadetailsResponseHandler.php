c3463201db8c6fb619561049ecc5dee9750f2160
rclaver <rclaver@xtec.cat>
2021-03-02 20:15:00 +0100

    añadir atributo revisión para mediadetails

--- cmd_response_handler/MediadetailsResponseHandler.php
+++ cmd_response_handler/MediadetailsResponseHandler.php
@@ -53,72 +53,68 @@ class MediadetailsResponseHandler extends WikiIocResponseHandler {
     }
 
     private function _responseDetail($requestParams, $responseData, &$ajaxCmdResponseGenerator) {
-        $mediado = "";
-        if ($requestParams['mediado']){
-            $mediado = $requestParams['mediado'];
+        $difftype = ($requestParams[MediaKeys::KEY_DIFFTYPE]) ? $requestParams[MediaKeys::KEY_DIFFTYPE] : "";
+        $mediado = ($requestParams[MediaKeys::KEY_MEDIA_DO]) ? $requestParams[MediaKeys::KEY_MEDIA_DO] : "";
+        $ajaxCmdResponseGenerator->addMediaDetails($difftype, 
+                                                   $mediado,
+                                                   "details", 
+                                                   $responseData[MediaKeys::KEY_ID],
+                                                   $responseData[MediaKeys::KEY_NS],
+                                                   $responseData['title'],
+                                                   $responseData['content'],
+                                                   $responseData[MediaKeys::KEY_REV]);
+
+        // Històric de versions
+        $propLlista = array(
+            'id' => $responseData['id'] . '_metaMediaDetailsProva',
+            'title' => "Històric de versions",
+            'ns' => $responseData['ns'],
+            'content' => $this->getModelAdapter()->mediaDetailsHistory($responseData['ns'], $responseData['image'])
+        );
+
+        // File Upload
+        $metaDataFileUpload = $this->getModelAdapter()->getMediaFileUpload();
+        $metaDataFileUpload['ns'] = $responseData['ns'];
+        $metaDataFileUpload['id'] = $responseData['id'] . '_metaMediafileupload';
+        if ($requestParams["versioupload"]){
+            $metaDataFileUpload['versioupload'] = $responseData['id'];
         }
-        $difftype = "";
-        if($requestParams['difftype']){
-            $difftype = $requestParams['difftype'];
+        $patrones = array();
+        //Los patrones 0 a 2 se ponen temporalmente para evitar que se modifique el nombre de archivo y el check
+        //de sobreescritura. Esto no será necesario cuando mediadetails disponga de un formulario propio para
+        //hacer un upload que sólo sirva para sustituir el fichero actual por uno nuevo (sin cambio de nombre)
+        $patrones[0] = '/type="text"/';
+        $patrones[1] = '/label for="upload__name"/';
+        $patrones[2] = '/label class="check"/';
+        $patrones[3] = '/dw__upload/';
+        $patrones[4] = '/upload__file/';
+        $patrones[5] = '/upload__name/';
+        $patrones[6] = '/dw__ow/';
+        $sustituciones = array();
+        $sustituciones[0] = 'type="text" readonly ';
+        $sustituciones[1] = 'label for="upload__name" style="display:none" ';
+        $sustituciones[2] = 'label class="check" style="display:none" ';
+        $sustituciones[3] = 'dw__upload_'.$responseData['id'];
+        $sustituciones[4] = 'upload__file_'.$responseData['id'];
+        $sustituciones[5] = 'upload__name_'.$responseData['id'];
+        $sustituciones[6] = 'dw__ow_'.$responseData['id'];
+        $metaDataFileUpload['content'] = preg_replace($patrones, $sustituciones, $metaDataFileUpload['content']);
+
+        $metaAgrupa = [$propLlista];
+        if ($responseData[MediaKeys::KEY_REV] === 0) {
+            $metaAgrupa[] = $metaDataFileUpload;
         }
-        if ($requestParams[MediaKeys::KEY_DELETE]) {
-            $ajaxCmdResponseGenerator->addMediaDetails($difftype, $mediado, MediaKeys::KEY_DELETE, $requestParams[MediaKeys::KEY_DELETE], $responseData['ns'], $requestParams['title'], $responseData['content']);
-        } else {
-            $ajaxCmdResponseGenerator->addMediaDetails($difftype, $mediado, "details", $responseData['id'], $responseData['ns'], $responseData['title'], $responseData['content']);
+        $ajaxCmdResponseGenerator->addMetaMediaDetailsData($responseData['id'], $metaAgrupa);
 
-            // Històric de versions
-            $propLlista = array(
-                'id' => $responseData['id'] . '_metaMediaDetailsProva',
-                'title' => "Històric de versions",
-                'ns' => $responseData['ns'],
-                'content' => $this->getModelAdapter()->mediaDetailsHistory($responseData['ns'], $responseData['image'])
-            );
-
-            // File Upload
-            $metaDataFileUpload = $this->getModelAdapter()->getMediaFileUpload();
-            $metaDataFileUpload['ns'] = $responseData['ns'];
-            $metaDataFileUpload['id'] = $responseData['id'] . '_metaMediafileupload';
-            if ($requestParams["versioupload"]){
-                $metaDataFileUpload['versioupload'] = $responseData['id'];
-            }
-            $patrones = array();
-            //Los patrones 0 a 2 se ponen temporalmente para evitar que se modifique el nombre de archivo y el check
-            //de sobreescritura. Esto no será necesario cuando mediadetails disponga de un formulario propio para
-            //hacer un upload que sólo sirva para sustituir el fichero actual por uno nuevo (sin cambio de nombre)
-            $patrones[0] = '/type="text"/';
-            $patrones[1] = '/label for="upload__name"/';
-            $patrones[2] = '/label class="check"/';
-            $patrones[3] = '/dw__upload/';
-            $patrones[4] = '/upload__file/';
-            $patrones[5] = '/upload__name/';
-            $patrones[6] = '/dw__ow/';
-            $sustituciones = array();
-            $sustituciones[0] = 'type="text" readonly ';
-            $sustituciones[1] = 'label for="upload__name" style="display:none" ';
-            $sustituciones[2] = 'label class="check" style="display:none" ';
-            $sustituciones[3] = 'dw__upload_'.$responseData['id'];
-            $sustituciones[4] = 'upload__file_'.$responseData['id'];
-            $sustituciones[5] = 'upload__name_'.$responseData['id'];
-            $sustituciones[6] = 'dw__ow_'.$responseData['id'];
-            $metaDataFileUpload['content'] = preg_replace($patrones, $sustituciones, $metaDataFileUpload['content']);
-
-            $metaAgrupa = array(
-                "0" => $propLlista,
-                "1" => $metaDataFileUpload
-            );
-
-            $ajaxCmdResponseGenerator->addMetaMediaDetailsData($responseData['id'], $metaAgrupa);
-
-            //Càrrega de la zona info de missatges
-            $info = array('id' => "bodyContent_tablist_{$responseData['id']}", 'duration' => -1, 'timestamp' => date('d-m-Y H:i:s'));
-            $info['type'] = 'success';
-            if ($mediado == "diff"){
-                $info['message'] = ' &nbsp;Es mostren les versions a comparar.';
-            }else{
-                $info['message'] = ' &nbsp;Imatge ' . $responseData['id'] . ' carregada.';
-            }
-            $ajaxCmdResponseGenerator->addInfoDta($info);
+        //Càrrega de la zona info de missatges
+        $info = array('id' => "bodyContent_tablist_{$responseData['id']}", 'duration' => -1, 'timestamp' => date('d-m-Y H:i:s'));
+        $info['type'] = 'success';
+        if ($mediado == "diff"){
+            $info['message'] = ' &nbsp;Es mostren les versions a comparar.';
+        }else{
+            $info['message'] = ' &nbsp;Imatge ' . $responseData['id'] . ' carregada.';
         }
+        $ajaxCmdResponseGenerator->addInfoDta($info);
     }
 
 }
