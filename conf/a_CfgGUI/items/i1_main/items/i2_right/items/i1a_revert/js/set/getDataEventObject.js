093b910526a97d71f93eaa7eb39c7a3a83eaa666
rclaver <rclaver@xtec.cat>
2021-03-19 15:06:23 +0100

    extraDataToSend

--- conf/a_CfgGUI/items/i1_main/items/i2_right/items/i1a_revert/js/set/getDataEventObject.js
+++ conf/a_CfgGUI/items/i1_main/items/i2_right/items/i1a_revert/js/set/getDataEventObject.js
@@ -1,9 +1,16 @@
 var globalState = this.dispatcher.getGlobalState();
 var id = globalState.getCurrentId();
-var contentToolActual = this.dispatcher.getContentCache(id).getMainContentTool();
+var pType = globalState.getContent(id).projectType;
+var contentCache = this.dispatcher.getContentCache(id);
+var contentToolActual = contentCache.getMainContentTool();
 var ns = contentToolActual.ns;
 _ret = {
     id: id,
     name: 'save',
-    extraDataToSend: {do: 'save_rev'}
+    extraDataToSend: {do:'save_rev',
+                      ns:ns,
+                      projectType:pType,
+                      projectOwner:contentCache.projectOwner,
+                      projectSourceType:contentCache.projectSourceType
+                  }
 };
--- conf/cfgArray.php
+++ conf/cfgArray.php
@@ -995,7 +995,7 @@ $_arrIocCfgGUI = array (
                   'autoSize' => true,
                   'visible' => false,
                   'iconClass' => '\'iocIconUndo\'',
-                  'getDataEventObject' => 'function(_data){var _ret=null; var globalState = this.dispatcher.getGlobalState();var id = globalState.getCurrentId();var contentToolActual = this.dispatcher.getContentCache(id).getMainContentTool();var ns = contentToolActual.ns;_ret = { id: id, name: \'save\', extraDataToSend: {do: \'save_rev\'}};return _ret;}',
+                  'getDataEventObject' => 'function(_data){var _ret=null; var globalState = this.dispatcher.getGlobalState();var id = globalState.getCurrentId();var pType = globalState.getContent(id).projectType;var contentCache = this.dispatcher.getContentCache(id);var contentToolActual = contentCache.getMainContentTool();var ns = contentToolActual.ns;_ret = { id: id, name: \'save\', extraDataToSend: {do:\'save_rev\', ns:ns, projectType:pType, projectOwner:contentCache.projectOwner, projectSourceType:contentCache.projectSourceType }};return _ret;}',
                 ),
               ),
             ),
